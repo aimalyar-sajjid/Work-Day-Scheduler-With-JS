b09a8b0af0ea65f23aa133dbd0f31b68d8d52f93
aimalyar-sajjid <93589944+aimalyar-sajjid@users.noreply.github.com>
2022-11-20 18:34:01 +0430

    First Update

---
+++ README.md
@@ -0,0 +1,22 @@
+# Work Day Scheduler
+
+## Description:
+This app can help you to save your daily schedule and do your jobs according to your schedule, It can tells you about the past, present and future tasks, you added to your scheduler, You can easily save and set your scheduler, Your saved task will not remove after refreshing the browser, because all staffs save in local storage.
+
+
+## Technology:
+This beautiful project created using:
+- HTML 5
+- CSS 3
+- JavaScript
+
+
+
+## Usage
+Get the code and run 'index.html' file on your browser.
+
+## Questions?
+Don't hesitate, contact me any time for any query:
+
+- [Email](mailto:aimalyar2018@gmail.com)
+- [Github](https://github.com/aimalyarsajjid)
---
+++ js/app.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+
+    // SET DATE
+    $("#date").html(moment().format("dddd, MMMM, Do"));
+
+
+    // DECLARING VARAIBLES
+    let now = new Date();
+    now.setHours("11");
+    var hour = now.getHours();
+
+    console.log(hour);
+
+
+
+       $(".event").each(function(){
+
+            // CHECKING FOR PAST
+           if(hour > $(this).data("time"))
+           {
+                $(this).attr("class", "past"); //setAttribute("class", "value")
+           }
+
+            // CHECKING FOR PRESENT
+            if(hour == $(this).data("time"))
+            {
+                $(this).attr("class", "present");
+            }
+
+            // CHECKING FOR FUTURE
+            if(hour < $(this).data("time"))
+            {
+                $(this).attr("class", "future");
+            }
+
+
+
+            // RETRIVING DATA FROM LOCAL STORAGE
+            var eventHour = $(this).data("time");
+
+            for(var i=0; i<localStorage.length; i++)
+            {
+                var localStorageKey = localStorage.key(i);
+                if(eventHour == localStorageKey)
+                {
+                    $(this).val(localStorage.getItem(localStorageKey));
+                }
+            }
+
+
+       });
+
+
+
+    //STORING EVETNS
+    $(".saveBtn").click(function(e){
+
+
+
+        var textarea = e.target.parentElement.parentElement.querySelector("textarea");
+        var key = textarea.getAttribute("data-time");
+        var value = textarea.value;
+
+
+        window.localStorage.setItem("date", moment().format("MM DD YYYY"));
+        window.localStorage.setItem(key, value);
+
+    });
+
+
+
+
+
+
+
+    // CLEAR STORAGE WHEN DATE CHANGED
+    if(moment().format("MM DD YYYY") > window.localStorage.getItem("date"))
+    {
+        window.localStorage.clear();
+    }
+
+
+});
+
+
+
+
+
